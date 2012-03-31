d59f6962b776620ea9b1f83f8eb705c8c614d965
Keith Sharp <keith.sharp@gmail.com>
2012-03-31 13:29:56 +0100

    Initial Commit

---
+++ GPX Pretty Printer/GpxPpDocument.h
@@ -0,0 +1,13 @@
+//
+//  GpxPpDocument.h
+//  GPX Pretty Printer
+//
+//  Created by Keith Sharp on 31/03/2012.
+//  Copyright (c) 2012 Passback IT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GpxPpDocument : NSDocument
+
+@end
---
+++ GPX Pretty Printer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GPX Pretty Printer
+//
+//  Created by Keith Sharp on 31/03/2012.
+//  Copyright (c) 2012 Passback IT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
